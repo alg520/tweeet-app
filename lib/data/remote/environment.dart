1739e64eef39f811fb233628d2e273cb9cc70ef9
dannndi <dandi.061100@gmail.com>
2021-09-20 14:13:15 +0700

    change url api

--- lib/data/remote/environment.dart
+++ lib/data/remote/environment.dart
@@ -1,6 +1,6 @@
 class Environments {
-  static const String PRODUCTION = 'http://192.168.1.5:3000/api/v1';
-  static const String DEV = 'http://192.168.1.5:3000/api/v1';
+  static const String PRODUCTION = 'https://tweeet-api.herokuapp.com/api/v1';
+  static const String DEV = 'https://tweeet-api.herokuapp.com/api/v1';
 }
 
 class ConfigEnvironments {
@@ -8,11 +8,11 @@ class ConfigEnvironments {
   static const List<Map<String, String>> _availableEnvironments = [
     {
       'env': Environments.DEV,
-      'url': 'http://192.168.1.5:3000',
+      'url': 'https://tweeet-api.herokuapp.com/',
     },
     {
       'env': Environments.PRODUCTION,
-      'url': 'http://192.168.1.5:3000',
+      'url': 'https://tweeet-api.herokuapp.com/',
     },
   ];
 
